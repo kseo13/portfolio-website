fc8d16dcbb55b15345214f6a03a43a1ee2628761
kseo13 <136879898+kseo13@users.noreply.github.com>
2023-07-25 23:44:14 -0700

    work.html update
    updated project page and contact links

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Hi! Welcome to my portfolio website.")
+console.log("Hi! Welcome to my portfolio website")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
